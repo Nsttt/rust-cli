4d049e44e91521b1bfae85e208ba3ef2a4fc2be2
Nestor <elnestinatorHD@gmail.com>
2022-04-09 12:49:20 +0200

    Init and test

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+fn main() {
+    let args = Cli::parse();
+
+}
