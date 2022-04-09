4b259eca6f84d63f400a68b278129385e55c22ee
Nestor <elnestinatorHD@gmail.com>
2022-04-09 12:51:59 +0200

    Make cli print contents

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,11 @@ struct Cli {
 }
 fn main() {
     let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("Failed to read file");
 
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
